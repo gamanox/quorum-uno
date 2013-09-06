41fdebc7acc3f66648308e17a1872923cf8562e9
Menchaca <archerdragon57@gmail.com>
2013-09-06 12:26:20 -0600

    Login funcionalidad

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def authenticate_user
-  	if session[:quorum_user_id].nil?
-  		redirect_to :action => 'login'
+  	if session[:quorum_user_id].to_i < 1
+  		redirect_to :controller=> 'sessions'
   	end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,23 @@
 class SessionsController < ApplicationController
-
   def login
+    if !params[:uname].blank? and !params[:password].blank?
+      usuario=Judge.where("uname=? and password=?",params[:uname],params[:password]).first
+      if(defined? usuario and !usuario.blank? and defined? usuario.id)
+        session[:quorum_user_id]=usuario.id
+        redirect_to :controller=>'judges'
+      else
+        session[:quorum_user_id]=0
+      end
+    else
+      session[:quorum_user_id]=0
+    end
   end
 
-  def login_attemp
-  	if !params[:uname].blank? and !params[:password].blank?
-  		usuario=Judge.where("uname='?' and password='?'",params[:uname],params[:password]).first
-  		if(defined? usuario and !usuario.blank?)
-			  session[:quorum_user_id]=usuario.id
-    		redirect_to :action => 'judges'
-    	else
-    		render "login"	
-    	end
-  	else
-  		flash[:notice] = "Invalid Username or Password"
-      	flash[:color]= "invalid"
-  		render "login"
-  	end
+  def login_attempt
   end
 
   def logout
-    session[:quorum_user_id] = nil
-    redirect_to :action => 'login'
+    session[:quorum_user_id] = 0
+    redirect_to :controller=> 'home'
   end
 end
